d7707c654fc23d98332d3471ddfe21638da356b9
Bhagwat Singh Chouhan <bhagwat.chouhan@vulpinecode.com>
2018-01-31 07:16:56 +0530

    Added templates structure.

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -1,12 +1,12 @@
 <?php
 // Common resources required by Yii Applications
-Yii::setAlias( 'common', dirname( __DIR__ ) );
-Yii::setAlias( 'themes', dirname( dirname( __DIR__ ) ) . '/themes/' );
-Yii::setAlias( 'uploads', dirname( dirname( __DIR__ ) ) . '/uploads/' );
-Yii::setAlias( 'widgets', dirname( dirname( __DIR__ ) ) . '/widgets/' );
+Yii::setAlias( '@common', dirname( __DIR__ ) );
+Yii::setAlias( '@themes', dirname( dirname( __DIR__ ) ) . '/themes/' );
+Yii::setAlias( '@widgets', dirname( dirname( __DIR__ ) ) . '/widgets/' );
+Yii::setAlias( '@templates', dirname( dirname( __DIR__ ) ) . '/templates' );
+Yii::setAlias( '@uploads', dirname( dirname( __DIR__ ) ) . '/uploads/' );
 
 // Yii Applications
-Yii::setAlias( 'console', dirname( dirname( __DIR__ ) ) . '/console' );
-Yii::setAlias( 'admin', dirname( dirname( __DIR__ ) ) . '/admin' );
-Yii::setAlias( 'frontend', dirname( dirname( __DIR__ ) ) . '/frontend' );
-?>
+Yii::setAlias( '@console', dirname( dirname( __DIR__ ) ) . '/console' );
+Yii::setAlias( '@admin', dirname( dirname( __DIR__ ) ) . '/admin' );
+Yii::setAlias( '@frontend', dirname( dirname( __DIR__ ) ) . '/frontend' );
